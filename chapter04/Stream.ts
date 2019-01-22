5796d98954d1a4f04d3c42b567669bed9294dddd
Dylan Cutler <DCtheTall@users.noreply.github.com>
2019-01-21 21:53:43 -0500

    more work on streams

--- chapter04/Stream.ts
+++ chapter04/Stream.ts
@@ -20,27 +20,49 @@ export namespace Stream {
     const isEmpty = <T>(S: Stream<T>) => (Util.force(S) === EmptyCell);
 
     const head = <T>(S: Stream<T>) => {
-        let s = Util.force(S)
+        let s = Util.force(S);
         return <T>s((h, t) => h);
     };
 
     const tail = <T>(S: Stream<T>) => {
-        let s = Util.force(S)
+        let s = Util.force(S);
         return <Stream<T>>s((h, t) => t);
     };
 
-    // TODO do not export
-    export const cons = <T>(e: T, S: Stream<T>): Stream<T> =>
-        Util.lazy(() => (f: Selector<T>) => f(e, S));
+    const cons = <T>(e: T, S: Stream<T>): Stream<T> =>
+        Util.lazy((): StreamCell<T> => (f: Selector<T>) => f(e, S));
 
-    export const append = <T>(s: Stream<T>, t: Stream<T>): Stream<T> =>
-        (isEmpty(s) ? t
-        : cons(head(s), append(tail(s), t)));
-}
+    // Concat two streams
+    export const concat = <T>(s: Stream<T>, t: Stream<T>): Util.LazyFunction<Stream<T>> =>
+        Util.lazy(() => {
+            let helper = Util.optimize((s: Stream<T>, t: Stream<T>): Stream<T> =>
+                (isEmpty(s) ? t
+                : <Stream<T>>Util.optRecurse(
+                    () => cons(head(s), <Stream<T>>Util.force(concat(tail(s), t))))));
+            return helper(s, t);
+        });
 
-let s = Stream.EmptyStream;
-for (let i = 0; i < 1e3; i++) {
-    s = Stream.cons(i, s);
+    // Take the first n elements in the stream
+    export const take = <T>(n: number, s: Stream<T>): Util.LazyFunction<Stream<T>> =>
+        Util.lazy(() => {
+            let helper = Util.optimize((n: number, s: Stream<T>): Stream<T> =>
+                (isEmpty(s) ? EmptyStream
+                : (n === 0 ? EmptyStream
+                : <Stream<T>>Util.optRecurse(
+                    () => cons(head(s), <Stream<T>>Util.force(take(n - 1, tail(s))))))));
+            return helper(n, s);
+        });
+
+    // Drop the first n elements in the stream
+    export const drop = <T>(n: number, s: Stream<T>): Util.LazyFunction<Stream<T>> =>
+        Util.lazy(() => {
+            let helper = Util.optimize((n: number, s: Stream<T>): Stream<T> =>
+                (isEmpty(s) ? EmptyStream
+                : (n === 0 ? s
+                : <Stream<T>>Util.optRecurse(
+                    () => Util.force(drop(n - 1, tail(s)))))));
+            return helper(n, s);
+        });
 }
 
 
